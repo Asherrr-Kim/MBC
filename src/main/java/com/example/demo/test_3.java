884ab45b05f5a6b225bec11a3b6bbb15b49f554d
JongWon <sily714@naver.com>
2024-11-22 17:47:40 +0900

    test_3

---
+++ src/main/java/com/example/demo/test_3.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class test_3 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 12;
+		int sum = a+b;
+
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
